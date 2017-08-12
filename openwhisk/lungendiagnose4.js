855b931e044ecc0615355a76fb97033301263210
HartmutSeitter <seitter@de.ibm.com>
2017-08-12 22:50:59 +0200

    Add files via upload

---
+++ openwhisk/lungendiagnose4.js
@@ -0,0 +1,43 @@
+case "infodaten":
+    return new Promise ((resolve, reject) => {
+        discovery.query({
+            environment_id: 'bd8821ab-4451-4a8d-ad72-85625d0d6cdf',
+            collection_id: 'ae611af9-9419-4a67-ab25-2868b21ea5e2',
+            query: "",
+            count: 3,
+            return: "title",
+            aggregation: "term(enriched_text.docSentiment.type,count:3)"                
+        },  function(err, response) {
+                if (err) {
+                    console.log('error:',err);
+                } else {
+                    console.log('discovery call successful');
+                    var matching_results = response.matching_results;
+                    var text2 = response.results[0].text;
+                      
+                    // create response to alexa
+                    var outtext = "Die Daten über Lungendiagnose enthalten derzeit " + 
+                                   matching_results + " Dokumente. " +
+                                   "In der Summe sind " + response.aggregations[0].results[0].matching_results +
+                                   " mit einem " + response.aggregations[0].results[0].key +
+                                   " Stimmungsbild verfasst und " +
+                                   response.aggregations[0].results[1].matching_results +
+                                   " mit einem " + response.aggregations[0].results[1].key +
+                                   " Stimmungsbild verfasst." + 
+                                   " Weitere Details kannst Du zum Beispiel mit der Frage nach Schlüsselwörter oder Enitäten erhalten";
+                    var response = {
+                        "version": "1.0",
+                        "response": {
+                        "shouldEndSession": true,
+                            "outputSpeech":{
+                                "type": "PlainText",
+                                "text": outtext
+                            }
+                        }
+                    }
+                    return resolve (response);
+                } 
+            }
+        );
+    });
+break;
---
+++ openwhisk/lungendiagnose5.js
@@ -0,0 +1,61 @@
+case "lungenanalyse":
+    var searchString = args.request.intent.slots.search.value;
+    if (searchString === 'undefined') {
+        outtext = "ich konnte leider keine Suchkriterien in deiner Anfrage erkennen ";
+        var response = {
+            "version": "1.0",
+            "response": {
+            "shouldEndSession": true,
+                "outputSpeech":{
+                    "type": "PlainText",
+                    "text": outtext
+                }
+            }
+        }
+        return  response;
+        // handle no search string entered
+    } else {
+        // now call discovery with search string
+        var queryString = {'natural_language_query':searchString};
+                   
+        return new Promise ((resolve, reject) => {
+            discovery.query({
+                environment_id: 'bd8821ab-4451-4a8d-ad72-85625d0d6cdf',
+                collection_id: 'ae611af9-9419-4a67-ab25-2868b21ea5e2',
+                query: queryString,
+                count: 3
+                            
+            },  function(err, response) {
+                    if (err) {
+                        console.log('error:',err);
+                    } else {
+                        console.log('discovery call successful');
+                        var matching_results = response.matching_results;
+                        var text2 = response.results[0].text;
+                        
+                        // create response to alexa
+                        var outtext = "Waston discovery hat in seinen Dokumenten folgende Informationen für die Frage: " + searchString +
+                                       "gefunden: "+
+                                       "Das Dokument enhält die Entität " +
+                                        response.results[0].enriched_text.entities[0].text +
+                                       " vom Entitätentyp: " + 
+                                       response.results[0].enriched_text.entities[0].type +
+                                       " . Der Inhalt des Dokument lautet:" +
+                                       response.results[0].text;
+                    var response = {
+                            "version": "1.0",
+                            "response": {
+                            "shouldEndSession": true,
+                                "outputSpeech":{
+                                    "type": "PlainText",
+                                    "text": outtext
+                                }
+                            }
+                        }
+                        return resolve (response);
+                    } 
+                }
+            );
+        });
+    }
+break; 
